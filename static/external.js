1cfc7ab6de10de5d9a5d974c506602ef5a0f7b2e
Ziyun Ge <ziyunge1999@gmail.com>
2023-05-20 01:16:19 -0700

    search function complete

--- static/external.js
+++ static/external.js
@@ -1,6 +1,40 @@
+function loadCompanyTab(event) {
+    console.log('get infos');
+    var reponse_text = event.target.responseText;
+    infos = JSON.parse(event.target.responseText);
+    console.log(reponse_text);
+    if (JSON.stringify(infos) === '{}') {
+
+    }
+    else {
+        console.log(infos['logo']);
+        document.getElementById('logo').src = infos['logo'];
+        document.getElementById('symbol').innerHTML = infos['ticker'];
+        document.getElementById('company-name').innerHTML = infos['name'];
+        document.getElementById('exchange-code').innerHTML = infos['exchange'];
+    }
+}
+
+function requestAPI(url, fun) {
+    var XHR = new XMLHttpRequest();
+
+    XHR.addEventListener("load", function (event) {
+        fun(event);
+    });
+
+    XHR.addEventListener("error", function(event) {
+        alert('something went wrong');
+    });
+
+    XHR.open("get", url, true);
+    XHR.send()
+}
+
 function submitSearch() {
     var token = document.getElementById('token').value;
     console.log(`submit search ${token}`);
+
+    requestAPI(`/api/v1/stock/profile2?symbol=${token}`, loadCompanyTab);
 }
 
 window.onload = function(){
